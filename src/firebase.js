d65c04a98a2795fadfbc4ca3b3007199b63b1baa
nachan28 <torajiro282828@gmail.com>
2023-02-10 22:33:48 +0900

    add: add firebase and set up firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GithubAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCelqmReB7_w2pmEijlvmU6R-6L0zoQgr0",
+  authDomain: "blog-bf777.firebaseapp.com",
+  projectId: "blog-bf777",
+  storageBucket: "blog-bf777.appspot.com",
+  messagingSenderId: "557974602173",
+  appId: "1:557974602173:web:94b0c1168ecab6f82612d4",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GithubAuthProvider();
+const db = getFirestore(app);
+
+export { auth, provider, db };
