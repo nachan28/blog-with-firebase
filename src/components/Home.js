ff6f879a997a2c83f12b7acc98eae6b2423e3e0d
nachan28 <torajiro282828@gmail.com>
2023-02-13 19:54:09 +0900

    add: add Home page contents

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          今はReact学習中です。トラちゃんが大好きです。可愛くって仕方がないです。お風呂に入ったほうがいいです。
+        </div>
+        <div className='nameAndDeleteButton'>
+          <h3>@nanako</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
